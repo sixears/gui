2d31a4f440eff361de2de4354c9a07a3ab52edb4
Mr. Fluffy <git@sixearsm>
2024-01-08 21:03:53 +0000

    _

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "nix configuration for gui things; shared X / Wayland";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/354184a; # master 2023-12-13
+    flake-utils.url = github:numtide/flake-utils/c0e246b9;
+    myPkgs          = {
+      url    = github:sixears/nix-pkgs/r0.0.3.0;
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, myPkgs }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system:
+      let
+        pkgs    = nixpkgs.legacyPackages.${system};
+        my-pkgs = myPkgs.packages.${system};
+      in
+        rec {
+          packages = flake-utils.lib.flattenTree (with pkgs; {
+            # https://fontawesome.com/icons
+            inherit font-awesome;
+          });
+        }
+    );
+}
+
