9dc56ecd8dfeaaa0d86ebee7afaff32b8cece923
Mr. Fluffy <git@sixearsm>
2024-07-31 18:29:18 +0000

    _

--- src/xkeysyms.nix
+++ src/xkeysyms.nix
@@ -15,7 +15,7 @@ while (<$fh>) {
   chomp;
 
   if ( my ($n,$c) = m!^#define\s+XK_(\S+)\s.*?(?:/\*\s*(\S.*\S)\s*\*/)?\s*$! ) {
-    if ( $c !~ /^\s*$/ ) {
+    if ( ($c//"") !~ /^\s*$/ ) {
       printf "%-32s\t# %s\n", $n, $c;
     } else {
       say $n;
